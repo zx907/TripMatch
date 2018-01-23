4ebaf4fdbbac848d4ab1e2fb6f5a53c28537edae
caveman <caveman2015@qq.com>
2018-01-23 09:43:09 +0800

    fff

--- tripmatch/trip_server.py
+++ tripmatch/trip_server.py
@@ -26,21 +26,26 @@ DEBUG = True
 # configuration
 APPLICATION_NAME = 'Trip Match'
 app = Flask(__name__)
+app.secret_key = ''.join(random.choice(
+    string.ascii_uppercase + string.digits) for x in range(32))
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # TRIPMATCH_SETTINGS = 'settings.txt'
 # app.config.from_object(__name__)
 # app.config.from_envvar('TRIPMATCH_SETTINGS', silent=True)
 
 # database engine and session
-engine = create_engine("sqlite:///"+ os.getcwd() +"/testdb.db", connect_args={'check_same_thread': False})
+engine = create_engine("sqlite:///" + os.getcwd() +
+                       "/testdb.db", connect_args={'check_same_thread': False})
 
 
 # create tables
 def init_db():
     Base.metadata.create_all(engine)
 
+
 def get_db_session():
     return sessionmaker(bind=engine)()
+
 
 def login_required(f):
     @wraps(f)
@@ -50,11 +55,13 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+
 @app.route('/')
 def timeline():
     db_session = get_db_session()
     trips = db_session.query(TripDetails).all()
     return render_template('timeline.html', trips=trips)
+
 
 @app.route('/public')
 def public_timeline():
@@ -62,10 +69,11 @@ def public_timeline():
     trips = db_session.query(TripDetails).all()
     return render_template('timeline.html', trips=trips)
 
+
 @app.route('/trip_detail/<int:trip_id>', methods=['GET', 'POST'])
 def display_trip(trip_id):
     db_session = get_db_session()
-    if request.method=='POST':
+    if request.method == 'POST':
         if login_session.get('user_id', None) == None:
             return redirect(url_for('login'))
         text = request.form['message']
@@ -78,13 +86,15 @@ def display_trip(trip_id):
             new_wtl_entry = Waitinglist(
                 user_id=user_id, trip_id=trip_id, text=text, post_date=post_date)
             db_session.add(new_wtl_entry)
-            db_session.commit() 
+            db_session.commit()
         except:
             db_session.rollback()
 
     trip = db_session.query(TripDetails).filter_by(id=trip_id).one()
-    waitinglist = db_session.query(Waitinglist).filter_by(trip_id=trip_id).all()
+    waitinglist = db_session.query(
+        Waitinglist).filter_by(trip_id=trip_id).all()
     return render_template('trip_details.html', trip=trip, waitinglist=waitinglist)
+
 
 @app.route('/new_trip', methods=['GET', 'POST'])
 def new_trip():
@@ -104,26 +114,26 @@ def new_trip():
             date_create = datetime.now().isoformat(' ')
 
             new_trip = TripDetails(user_id=login_session['user_id'],
-                                destination_id=destination.id,
-                                duration=request.form['duration'],
-                                date_start=request.form['date_start'],
-                                companions=request.form['companions'],
-                                city_takeoff=request.form['city_takeoff'],
-                                expected_group_size=request.form['expected_group_size'],
-                                notes=request.form['notes'],
-                                date_create=date_create
-                                )
+                                   destination_id=destination.id,
+                                   duration=request.form['duration'],
+                                   date_start=request.form['date_start'],
+                                   companions=request.form['companions'],
+                                   city_takeoff=request.form['city_takeoff'],
+                                   expected_group_size=request.form['expected_group_size'],
+                                   notes=request.form['notes'],
+                                   date_create=date_create
+                                   )
 
             db_session.add(new_trip)
             db_session.commit()
 
             # if 'file' not in request.files:
             #     flash('No file part')
-                
+
             file = request.files['file']
             # if file.filename=='':
             #     flash('No selected file')
-                
+
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -136,6 +146,7 @@ def new_trip():
 
     return render_template('new_trip.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     # Log user in and save logged-in username in login_session dict
@@ -145,7 +156,7 @@ def login():
     if request.method == 'POST':
         db_session = get_db_session()
         user = db_session.query(Users).filter(
-            Users.username==request.form['username']).first()
+            Users.username == request.form['username']).first()
         if user == None:
             error = 'invalid username'
             flash('invalid username')
@@ -153,21 +164,23 @@ def login():
             error = 'invalid password'
             flash('invalid password')
         # Add a cookie to response obj
-        elif request.form['remember_me']:   
-            resp = make_response(redirect(url_for('timeline')))
-            resp.set_cookie('tripmatch_user_id', user.id, 3600)
-            login_session['user_id'] = user.id
-            return resp
+        # elif request.form.get('remember_me', None) :
+        #     print('set cookie')
+        #     resp = make_response(redirect(url_for('timeline')))
+        #     resp.set_cookie('tripmatch_user_id', str(user.id))
+        #     login_session['user_id'] = user.id
+        #     return resp
         else:
             flash('you were logged in')
             login_session['user_id'] = user.id
             return redirect(url_for('timeline'))
     return render_template('login.html')
 
+
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     del login_session['user_id']
-    if request.cookie.get('user_id'):
+    if request.cookie.get('user_id', None):
         resp = make_response(redirect(url_for('public_timeline')))
         resp.set_cookie('tripmatch_user_id', user.id, 0)
         flash('You were logged out')
@@ -178,6 +191,7 @@ def logout():
         print('logout successful')
         return redirect(url_for('public_timeline'))
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if login_session.get('user_id', None) != None:
@@ -186,14 +200,14 @@ def register():
 
     #Todo: hashtable replace if-else
 
-    if request.method=='POST':
+    if request.method == 'POST':
         if not request.form['username']:
             error = 'Please enter a username'
-        elif UsernameExists(request.form['username']):
+        elif username_exists(request.form['username']):
             error = 'The username is already taken'
         elif not request.form['email']:
             error = 'Please enter an email address'
-        elif EmailExists(request.form['email']):
+        elif email_exists(request.form['email']):
             error = 'The email address is already taken'
         elif not request.form['password']:
             error = 'Please enter a password'
@@ -206,46 +220,52 @@ def register():
                     username=request.form['username'], email=request.form['email'], password=generate_password_hash(request.form['password']))
                 db_session.add(new_user)
                 db_session.commit()
-                flash('Register successfully')  
-                return redirect(url_for('timeline'))      
-            except :
+                flash('Register successfully')
+                return redirect(url_for('timeline'))
+            except:
                 db_session.rollback()
-                flash('Registion failed') 
+                flash('Registion failed')
                 return redirect(url_for('timeline'))
 
     return render_template('register.html')
 
-def EmailExists(email):
+
+def email_exists(email):
     db_session = get_db_session()
     return db_session.query(Users).filter(Users.email == email).first()
 
-def UsernameExists(username):
+
+def username_exists(username):
     db_session = get_db_session()
     return db_session.query(Users).filter(Users.username == username).first()
+
 
 def get_or_create(session, model, defaults=None, **kwargs):
     instance = session.query(model).filter_by(**kwargs).first()
     if instance:
         return instance, False
     else:
-        params = dict((k, v) for k, v in kwargs.items() if not isinstance(v, ClauseElement))
+        params = dict((k, v) for k, v in kwargs.items()
+                      if not isinstance(v, ClauseElement))
         params.update(defaults or {})
         instance = model(**params)
         session.add(instance)
         session.commit()
         return instance, True
 
+
 def allowed_file(filename):
     return '.' in filename and filename.split('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/upload_trip_img', methods=['GET', 'POST'])
 def upload_trip_img():
-    if request.method=='POST':
+    if request.method == 'POST':
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        if file.filename=='':
+        if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
@@ -255,7 +275,5 @@ def upload_trip_img():
 
 
 if __name__ == '__main__':
-    app.secret_key = ''.join(random.choice(
-        string.ascii_uppercase + string.digits) for x in range(32))
     init_db()
     app.run(host='127.0.0.1', port=5000, debug=True)
