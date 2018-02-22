3b06c2ab2a31cd3baf26ef49417421eab4470d07
caveman <caveman2015@qq.com>
2018-02-22 15:07:44 +0800

    restful api is working now

--- tripmatch/model/tripmatch_model.py
+++ tripmatch/model/tripmatch_model.py
@@ -5,6 +5,7 @@ from sqlalchemy import Table
 
 Base = declarative_base()
 
+
 class Users(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
@@ -15,14 +16,17 @@ class Users(Base):
     trip_details = relationship('TripDetails', back_populates='users')
     waitinglist = relationship('Waitinglist', back_populates='users')
 
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
     def __repr__(self):
-        return "<User(username='%s', email='%s', password='%s')>" % (self.name, self.fullname, self.password)
+        return "<User(username='%s', email='%s', password='%s')>" % (self.username, self.email, self.password)
 
+    user_dest_association_tbl = Table('usr_dest_as_tbl', Base.metadata,
+                                      Column('user_id', Integer, ForeignKey('users.id')),
+                                      Column('destination_id', Integer, ForeignKey('destinations.id'))
+                                      )
 
-user_dest_association_tbl = Table('usr_dest_as_tbl', Base.metadata,
-    Column('user_id', Integer, ForeignKey('users.id')),
-    Column('destination_id', Integer, ForeignKey('destinations.id'))
-)
 
 class TripDetails(Base):
     """username, country, state, destination, duration, date_start, companions, city_takeoff"""
@@ -44,9 +48,13 @@ class TripDetails(Base):
     destinations = relationship('Destinations', back_populates='trip_details')
     waitinglist = relationship('Waitinglist', back_populates='trip_details')
 
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
     def __repr__(self):
         "<TripDetails(username='%s', destination='%s', duration='%s', date_start='%s', companion='%s', city_takeoff='%s', expected_group_size='%s', notes='%s')>" \
-        % (self.username, self.destination, self.duration, self.date_start, self.companions, self.city_takeoff, self.expected_group_size, self.notes)
+        % (self.username, self.destination, self.duration, self.date_start, self.companions, self.city_takeoff,
+           self.expected_group_size, self.notes)
 
 
 class Destinations(Base):
@@ -56,6 +64,9 @@ class Destinations(Base):
     destination = Column(String, nullable=False)
 
     trip_details = relationship('TripDetails', back_populates='destinations')
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
 class Waitinglist(Base):
@@ -69,9 +80,18 @@ class Waitinglist(Base):
     users = relationship('Users', back_populates="waitinglist")
     trip_details = relationship('TripDetails', back_populates="waitinglist")
 
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
-# class TripToDestination(Base):
-#     __tablename__ = 'user_to_desination'
-#     id = Column(Integer, primary_key=True)
-#     user_id = Column(Integer, ForeignKey('users.id'))
-#     destination = Column(String, ForeignKey('destinations.id')) 
+
+class InMails(Base):
+    __tablename__ = 'inmails'
+    id = Column(Integer, primary_key=True)
+    from_id = Column(Integer, nullable=False)
+    to_id = Column(Integer, nullable=False)
+    inmail_title = Column(String, nullable=False)
+    inmail_text = Column(String, nullable=False)
+    send_date = Column(String, nullable=False)
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
--- tripmatch/trip_server.py
+++ tripmatch/trip_server.py
@@ -5,14 +5,14 @@ from flask import session as login_session
 from flask import make_response, Response, request
 from flask import json, jsonify
 from flask import abort
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, marshal
 from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import ClauseElement
 from model.tripmatch_model import Base, Users, TripDetails, Waitinglist, Destinations
 from werkzeug.security import check_password_hash, generate_password_hash, gen_salt
 from werkzeug.utils import secure_filename
-from jinja2 import TemplateNotFound
 from datetime import datetime, timedelta
 from functools import wraps
 import random
@@ -161,9 +161,63 @@ def new_trip():
     return render_template('new_trip.html')
 
 
-@app.route('/edit_trip/<int:trip_id>')
+# Reuse same template as new_trip, but prefill forms with existing infomation
+@app.route('/edit_trip/<int:trip_id>', methods=['GET', 'POST'])
 def edit_trip(trip_id):
-    return "skip"
+    app.logger.info('edit_trip function')
+    if 'user_id' not in login_session:
+        redirect(url_for('login'))
+
+
+    # post new trip
+    if request.method == 'POST':
+
+        db_session = Session()
+        try:
+            destination = get_or_create(
+                db_session,
+                Destinations,
+                destination=request.form['destination']
+            )[0]
+
+            date_create = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+
+            new_trip = TripDetails(user_id=login_session['user_id'],
+                                   destination_id=destination.id,
+                                   duration=request.form['duration'],
+                                   date_start=request.form['date_start'],
+                                   companions=request.form['companions'],
+                                   city_takeoff=request.form['city_takeoff'],
+                                   expected_group_size=request.form['expected_group_size'],
+                                   notes=request.form['notes'],
+                                   date_create=date_create)
+
+            # if no image is to upload, new_trip.ima_name = None
+            if 'new_trip_img_file' not in request.files:
+                app.logger.info('No file')
+
+            file = request.files['new_trip_img_file']
+            app.logger.info('original filename: {}'.format(file.filename))
+
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                new_trip.img_name = filename
+                app.logger.info('uploaded filename in new_trip: {}'.format(new_trip.img_name))
+            else:
+                new_trip.img_name = None
+                app.logger.info('should be empty filename: {}'.format(new_trip.img_name))
+
+            db_session.add(new_trip)
+            db_session.commit()
+            flash('Your trip is posted')
+        # todo, specify what exception is expected here
+        except Exception as e:
+            db_session.rollback()
+            app.logger.info(e)
+            flash('Failed to post your trip')
+
+    return render_template('new_trip.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -306,15 +360,26 @@ def upload_trip_img():
             return redirect(url_for('uploaded file', filename=filename))
 
 
-@app.route('/manage')
-@app.route('/manage/profile')
+@app.route('/manage', methods=['GET', 'POST'])
+@app.route('/manage/profile', methods=['GET', 'POST'])
 def manage_profile():
     # if login_session.get('user_id', None):
     #     app.logger.info('login_session user_id: {}'.format(login_session['user_id']))
     #     return redirect(url_for('timeline'))
-    db_session = Session()
-    user = db_session.query(Users).filter(Users.id == login_session['user_id']).first()
-    return render_template('manage_profile.html', user=user)
+
+    if request.method == 'POST':
+        db_session = Session()
+        try:
+            user = db_session.query(Users).filter(Users.id == login_session['user_id']).first()
+            user.email = request.form['NewEmail']
+            user.password = generate_password_hash(request.form['NewPassword'])
+            db_session.commit()
+            flash('Profile updated successfully')
+        except SQLAlchemyError:
+            db_session.rollback()
+            flash('Failed to update profile info')
+
+    return render_template('manage_profile.html')
 
 
 # @app.context_processor
@@ -339,50 +404,59 @@ def utility_processor():
         return (datetime.strptime(start_date, '%Y-%m-%d') + timedelta(int(duration))).strftime('%Y-%m-%d')
     return dict(calc_date_end=calc_date_end)
 
-# Restful api part
+# Restful api
 class UserAPI(Resource):
     def get(self, user_id):
+        db_session = Session()
         user = db_session.query(Users).filter_by(id=user_id).one()
-        return user
+        return jsonify(user.to_dict())
 
     def put(self, user_id):
+        db_session = Session()
         user = db_session.query(Users).filter_by(id=user_id).one()
-        return user
+        return jsonify(user.to_dict())
 
     def delete(self, user_id):
+        db_session = Session()
         user = db_session.query(Users).filter_by(id=user_id).one()
         if not user:
-            session.delete(user)
+            db_session.delete(user)
 
 
 class TripAPI(Resource):
     def get(self, trip_id):
-        trip = db_session.query(TripsDetails).filter_by(id=trip_id).one()
-        return trip
+        db_session = Session()
+        trip = db_session.query(TripDetails).filter_by(id=trip_id).one()
+        return jsonify(trip.to_dict())
 
     def put(self, trip_id):
-        trip = db_session.query(TripsDetails).filter_by(id=trip_id).one()
-        return trip
+        db_session = Session()
+        trip = db_session.query(TripDetails).filter_by(id=trip_id).one()
+        return jsonify(trip.to_dict())
 
     def delete(self, trip_id):
-        trip = db_session.query(TripsDetails).filter_by(id=trip_id).one()
+        db_session = Session()
+        trip = db_session.query(TripDetails).filter_by(id=trip_id).one()
         if not trip:
-            session.delete(trip)
+            db_session.delete(trip)
 
 
 class DestinationAPI(Resource):
     def get(self, destination_id):
-        destination = db_session.query(TripsDetails).filter_by(id=destination_id).one()
-        return destination
+        db_session = Session()
+        destination = db_session.query(TripDetails).filter_by(id=destination_id).one()
+        return jsonify(destination.to_dict())
 
     def put(self, destination_id):
-        destination = db_session.query(TripsDetails).filter_by(id=destination_id).one()
-        return destination
+        db_session = Session()
+        destination = db_session.query(TripDetails).filter_by(id=destination_id).one()
+        return jsonify(destination.to_dict())
 
     def delete(self, destination_id):
-        destination = db_session.query(TripsDetails).filter_by(id=destination_id).one()
+        db_session = Session()
+        destination = db_session.query(TripDetails).filter_by(id=destination_id).one()
         if not destination:
-            session.delete(destination)
+            db_session.delete(destination)
 
 
 api.add_resource(UserAPI, '/user_api/<int:user_id>')
